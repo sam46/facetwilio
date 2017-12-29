f5d550afbca2150ba807e39f8961f61d383d010b
sam46 <abdlwahdsa@gmail.com>
2017-12-29 02:47:03 -0800

    add back creds.py

---
+++ creds.py
@@ -0,0 +1,13 @@
+# fill in your fb and twilio credentials here:
+
+fb = {
+	'email': '', 
+	'pwd': ''
+}
+
+twilio = { 
+	'phone': '',  # '+1xxxxxxxxxx'
+	'twilio_phone': '', # '+1xxxxxxxxxx'
+	'sid': '', 
+	'auth': '' # auth token
+}
