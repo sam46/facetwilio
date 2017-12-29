ff3a052da2f8145574e97aa5bc135941179833e4
sam46 <abdlwahdsa@gmail.com>
2017-12-29 02:11:38 -0800

    inital commit

---
+++ fb2twilio.py
@@ -0,0 +1,26 @@
+import creds
+import threading
+from fbchat import Client
+from fbchat.models import *
+from twilio.rest import Client as TwilioClient
+
+
+def forwardMsg(msg):
+    tclient.messages.create(
+        to=creds.twilio['phone'],
+        from_=creds.twilio['twilio_phone'],
+        body=str(msg)
+    )
+
+class CustomClient(Client):
+    def onMessage(self, mid, author_id, message_object, thread_id, thread_type, ts, metadata, msg, **kwargs):
+        author = self.fetchUserInfo(author_id).values()[0]
+        print 'new msg from ({:s}):'.format(author.name), msg['delta']['body']
+        thr = threading.Thread(target=forwardMsg, args=['({:s}):'.format(author.name)+ msg['delta']['body']])
+        # thr.start()
+
+if __name__ == '__main__':
+    tclient = TwilioClient(creds.twilio['sid'], creds.twilio['auth'])
+    fclient = CustomClient(creds.fb['email'], creds.fb['pwd'])
+    fclient.listen()
+    # fclient.logout()
---
+++ twilio2fb.py
@@ -0,0 +1,28 @@
+import creds
+import threading
+from fbchat import Client
+from fbchat.models import *
+from flask import Flask, request, redirect
+# from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route('/sms', methods=['GET', 'POST'])
+def smsHandler():
+    try:
+        if request.form['From'] == creds.twilio['phone']:
+            print request.form['Body']
+            sms = request.form['Body'].split(':')
+            name, msg = sms[0], ''.join(sms[1:])
+            user = fclient.searchForUsers(name)[0]
+            fclient.send(Message(text=msg), thread_id=user.uid, thread_type=ThreadType.USER)
+    except:
+        print 'exception @ smsHandler!'
+    # resp = MessagingResponse()
+    # resp.message('msg123')
+    # print str(resp)
+    return ''
+    
+if __name__ == '__main__':
+    fclient = Client(creds.fb['email'], creds.fb['pwd'])
+    app.run(debug=True)
